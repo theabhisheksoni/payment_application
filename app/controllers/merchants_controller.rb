d884f5a78e132740b0995a5b3a1f0b703ab396d1
Abhishek soni <sonijiabhishek@gmail.com>
2023-01-07 19:13:06 +0530

    Add Merchants controller and views

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,38 @@
+class MerchantsController < ApplicationController
+  before_action :set_merchant, except: :index
+
+  def index
+    @merchants = current_user.merchants
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @merchant.update(merchant_params)
+      redirect_to @merchant
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @merchant.destroy
+      flash[:notice] = 'Merchant deleted successfully'
+    else
+      flash[:error] = @merchant.errors.full_messages.join(',')
+    end
+    redirect_to merchants_path
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name, :email, :description, :status)
+  end
+
+  def set_merchant
+    @merchant = current_user.merchants.find_by(id: params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  resources :merchants, except: :create
   devise_for :users
   namespace :api do
     namespace :v1 do
